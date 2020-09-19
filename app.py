e6f760650dbeb0f1a6ff64f45aaec111d19e810a
JorgePerC <42452493+JorgePerC@users.noreply.github.com>
2020-09-19 12:21:37 -0500

    Avances
    Co-authored-by: alexfh2001@gmail.com <alexfh2001@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import *
-import pyrebase
+from flask import Flask
+from flask import render_template
+# import pyrebase
 
 app = Flask(__name__)
 
@@ -7,6 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/hola")
+def hola():
+    lol =  20
+    lol += 500
+    return "hola, xd " + str(lol)
+
+@app.route("/pruba")
+def aprubas():
+    return render_template("prubas.html", titulo = "este es un titulo", body1 = "parte 1 del juego", body2 = 'parte 2 del juego')
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
+    
